64b5fcd97a452a925e3f5423962c679caa54cc5d
Sunil.Jacob <sunil1405@gmail.com>
2020-03-05 05:23:09 +0530

    Node JS Program for Auth0

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+
+const app = express();
+const port = 8080;
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://ivybridge.auth0.com/.well-known/jwks.json',
+  }),
+  audience: 'http://localhost:8080',
+  issuer: 'https://ivybridge.auth0.com/',
+  algorithms: [ 'RS256' ],
+});
+
+app.use(cors());
+app.get('/', checkJwt,  (req, res) => res.send({hello: 'world'}));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
